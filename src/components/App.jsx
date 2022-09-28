4d7a23a9664fdb12e486a89435cf3736abfdb952
Volodymyr Velch <velch83@gmail.com>
2022-09-28 23:05:31 +0300

    markup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,71 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from "react";
+import { Main,FeddbackBtn,BtnBlock } from "./App.styled";
+
+export class App extends React.Component{
+state = {
+  good: 0,
+  neutral: 0,
+  bad: 0
+  }
+  clickGoodBtn = () => {
+    this.setState(prevState => ({
+      good: prevState.good += 1,
+    }));
+  }
+  clickNaturalBtn = () => {
+    this.setState(prevState => ({
+      neutral: prevState.neutral += 1,
+    }));
+  }
+  clickBadBtn = () => {
+    this.setState(prevState => ({
+      bad: prevState.bad += 1,
+    }));
+  }
+  
+  
+  countTotalFeedback() {
+    return Object.values(this.state).reduce((a,b )=>a+b)
+   }
+  countPositiveFeedbackPercentage() { 
+    return parseInt((this.state.good/this.countTotalFeedback())*100) 
+  };
+  
+  render() {
+    return (
+      <Main>
+        <h2>Please leave feedback</h2>
+        <BtnBlock>
+          <FeddbackBtn type="button" onClick={this.clickGoodBtn}>Good</FeddbackBtn>
+          <FeddbackBtn type="button" onClick={this.clickNaturalBtn}>Netural</FeddbackBtn>
+          <FeddbackBtn type="button" onClick={this.clickBadBtn}>Bad</FeddbackBtn>
+        </BtnBlock>
+        <section title="Statistics">
+          {this.countTotalFeedback() > 0 ? (
+        <div>
+        <h3>Statistic</h3>
+        <ul>
+          <li>Good: {this.state.good }</li>
+          <li>neutral: {this.state.neutral }</li>
+          <li>Bad: {this.state.bad }</li>
+          <li>Total: {this.countTotalFeedback()} </li>
+          <li>Positive Feedback: {this.countPositiveFeedbackPercentage() ? this.countPositiveFeedbackPercentage() : 0 }  %</li>
+        </ul>
+            </div>
+            
+          ): (
+            <div message="There is no feedback yet">There is no feedback yet</div>
+          )
+          }
+
+
+        </section>
+      </Main>
+    );
+  }
+}
+
+
+
+
+
---
+++ src/components/App.styled.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Main = styled.div`
+        height: 100vh;
+        /* display: flex; */
+        justify-content: center;
+        align-items: center;
+        padding: 20px;
+        font-size: 40;
+        color:#010101;
+`;
+
+export const BtnBlock = styled.div`
+   gap: 10px;
+   padding: 15px;
+        
+`
+
+export const FeddbackBtn = styled.button`
+  padding: 5px;
+  margin: 10px;
+  width: 60px;
+`
